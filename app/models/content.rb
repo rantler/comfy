8aa3f27a6536dbb00855c1ffcf545251e93aa9eb
Randy Antler <tantalusrur@gmail.com>
2015-09-27 17:30:00 -0700

    ENG: Make accessible_properties recursive

--- app/models/content.rb
+++ app/models/content.rb
@@ -2,8 +2,16 @@ class Content < ActiveRecord::Base
 
   serialize :properties
 
-  def accessible_properties
-    OpenStruct.new(properties)
+  def accessible_properties(hash = properties)
+    if hash.is_a?(Hash)
+      hash.each do |key, value|
+        hash[key] = accessible_properties(value)
+      end
+      hash = OpenStruct.new(hash)
+    elsif hash.is_a?(Array)
+       hash = hash.map { |array| accessible_properties(array) }
+    end
+    hash
   end
 
 end
